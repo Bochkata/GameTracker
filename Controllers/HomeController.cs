9f99956f9cbec14159cb92613882ce1e98b18dd0
Boris Iskrenov <91016936+Bochkata@users.noreply.github.com>
2022-12-05 17:25:47 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using GameTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GameTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
