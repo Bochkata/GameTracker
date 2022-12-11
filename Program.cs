b9e5c60c6ad7538fa1d7273705ec9e0c1407d96e
Boris Iskrenov <91016936+Bochkata@users.noreply.github.com>
2022-12-11 21:03:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+using GameTracker.Contracts;
+using GameTracker.Data;
+using GameTracker.Data.Entities;
+using GameTracker.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<GameTrackerDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<User>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+}).AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<GameTrackerDbContext>();
+
+builder.Services.AddScoped<IBoardGameService, BoardGameService>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IBookGameService, BookGameService>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IComputerGameService, ComputerGameService>();
+builder.Services.AddControllersWithViews();
+
+builder.Services.ConfigureApplicationCookie(opt =>
+{
+    opt.LoginPath = "/User/Login";
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
